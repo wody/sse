63a1a082956027ba008d4fc91fc5583628bdcec9
valli <stefan.vallaster@gmail.com>
2010-12-04 21:40:04 +0100

    minor changes
    Signed-off-by: valli <stefan.vallaster@gmail.com>

--- sse-client/src/sse/client/controller/CustomerController.java
+++ sse-client/src/sse/client/controller/CustomerController.java
@@ -11,70 +11,73 @@ import sse.model.Customer;
 @ManagedBean("customerCtrl")
 @SessionScoped
 public class CustomerController {
-	
-	
+
 	public CustomerController() {
-		
+
 		customers = new ArrayList<Customer>();
-		
-		for(int i = 0; i < 10; i++) {
+
+		for (int i = 0; i < 10; i++) {
 			Customer c = new Customer();
-			
+
 			c.setBillingAdress("Address " + i);
 			c.setCity("City" + i);
 			c.setCompany("com");
 			c.setDiscount(0.5);
 			c.seteMail("mail");
 			c.setFax("fax");
-			c.setName("Customer Name" +i);
+			c.setName("Customer Name" + i);
 			c.setNote("note");
 			c.setPhone("phone");
 			c.setSelected(false);
-			c.setSurname("SURNAME" +i);
+			c.setSurname("SURNAME" + i);
 			c.setWeb("web");
 			c.setZip("COG1");
-			
+
 			customers.add(c);
 		}
-		
-		selected = customers.get(customers.size()-1);
-		
+
+		selected = customers.get(customers.size() - 1);
+
 	}
-	
+
 	private List<Customer> customers;
 	private Customer selected;
 	private Boolean delete;
-	
+
 	public List<Customer> getCustomers() {
 		return customers;
 	}
+
 	public void setCustomers(List<Customer> customers) {
 		this.customers = customers;
 	}
+
 	public Customer getSelected() {
 		return selected;
 	}
+
 	public void setSelected(Customer selected) {
 		this.selected = selected;
 	}
-	
+
 	public void create() {
-		
+
 	}
-	
+
 	public void save() {
-		
+
 	}
-	
+
 	public void delete() {
 		this.setDelete(Boolean.TRUE);
 	}
-	
+
 	public void setDelete(Boolean delete) {
 		this.delete = delete;
 	}
+
 	public Boolean getDelete() {
 		return delete;
 	}
-	
+
 }
