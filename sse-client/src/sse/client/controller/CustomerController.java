9ac68860c73c64f0a8edf4ef9bf6bc7942ec730d
Andrea Fueresz <andrea.f1@gmx.at>
2010-12-05 19:25:40 +0100

    customerDetail

--- sse-client/src/sse/client/controller/CustomerController.java
+++ sse-client/src/sse/client/controller/CustomerController.java
@@ -16,6 +16,18 @@ public class CustomerController {
 	
 	@EJB
 	private CustomerDAO dao;
+    private String name;
+    private String surname;
+	private String billingAdress;
+    private String zip;
+    private String city;
+	private String company;
+	private String note;
+	private Double discount;
+	private String phone;
+	private String eMail;
+	private String web;
+	private String fax;
 
 	public CustomerController() {
 
@@ -63,18 +75,50 @@ public class CustomerController {
 
 	public void setSelected(Customer selected) {
 		this.selected = selected;
+		System.out.println("DEBUG SELECTED CUSTOMER "+selected);
+		this.name = selected.getName();
+		this.surname = selected.getSurname();
+		this.billingAdress = selected.getBillingAdress();
+		this.zip = selected.getZip();
+		this.city = selected.getCity();
+		this.company = selected.getCompany();
+		this.note = selected.getNote();
+		this.discount = selected.getDiscount();
+		this.phone = selected.getPhone();
+		this.eMail = selected.getPhone();
+		this.web = selected.getWeb();
+		this.fax = selected.getFax();
 	}
 
 	public void create() {
-
+		this.name = "";
+		this.surname = "";
+		this.billingAdress = "";
+		this.zip = "";
+		this.city = "";
+		this.company = "";
+		this.note = "";
+		this.discount = 0d;
+		this.phone = "";
+		this.eMail = "";
+		this.web = "";
+		this.fax = "";
 	}
 
 	public void save() {
 		Customer c = new Customer();
-		c.setName("Hallo");
-		c.setSurname("Ich");
-		c.setBillingAdress("heisse");
-		c.setCity("TESTPERSON");
+		c.setName(name);
+		c.setSurname(surname);
+		c.setBillingAdress(billingAdress);
+		c.setZip(zip);
+		c.setCity(city);
+		c.setCompany(company);
+		c.setNote(note);
+		c.setDiscount(discount);
+		c.setPhone(phone);
+		c.seteMail(eMail);
+		c.setWeb(web);
+		c.setFax(fax);
 		dao.save(c);
 	}
 
@@ -90,4 +134,109 @@ public class CustomerController {
 		return delete;
 	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public CustomerDAO getDao() {
+		return dao;
+	}
+
+	public void setDao(CustomerDAO dao) {
+		this.dao = dao;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getBillingAdress() {
+		return billingAdress;
+	}
+
+	public void setBillingAdress(String billingAdress) {
+		this.billingAdress = billingAdress;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	public Double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(Double discount) {
+		this.discount = discount;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	public String getWeb() {
+		return web;
+	}
+
+	public void setWeb(String web) {
+		this.web = web;
+	}
+
+	public String getFax() {
+		return fax;
+	}
+
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+
+	
 }
